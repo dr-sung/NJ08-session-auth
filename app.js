440bd927d50df44762646fa76e686375253afbef
Hong Sung <profsung@gmail.com>
2018-03-05 11:00:06 -0600

    revised commit

--- app.js
+++ app.js
@@ -14,9 +14,10 @@ app.use(session({
 // Authentication & Authorization middleware
 const auth = function(req, res, next) {
     // hardcoded authentication in this demo => username: test, password: ppp 
-    // in the real app, you will lookup the Database for username/password info
+	// in the real app, you will lookup the Database for username/password info
+	console.log('@auth middleware', req.session.user);
     if (req.session && req.session.user
-        && req.session.user.name == 'test' && req.session.user.pass == 'ppp') {
+        && req.session.user.name == 'test' && req.session.user.password == 'ppp') {
                 return next();
     } else {
        res.redirect('/error'); // or redirect to login page
@@ -29,28 +30,24 @@ app.get('/', (req, res) => {
 
 // login prompt endpoint
 app.get('/login', (req, res) => {
-    if (req.session && req.session.user) {
-        return res.redirect('/userhome');
-    } else {
-        let page =
-        `<html><head><title>Login</title></head><body>
-         <h2>Login Page</h2>
-        <form action="/login" method="post">
-          username: <input type="text" name="name" size="20"><br/>
-          password: <input type="password" name="pass" size="20"><br/>
-           <input type="submit" value="Login">
-        </form>
-        </body></html>`
-        res.send(page);
-    }
+	let page =
+		`<html><head><title>Login</title></head><body>
+			<h2>Login Page</h2>
+		<form action="/login" method="post">
+			username: <input type="text" name="name" size="20"><br/>
+			password: <input type="password" name="pass" size="20"><br/>
+			<input type="submit" value="Login">
+		</form>
+		</body></html>`;
+	return res.send(page);
 });
 
 // login endpoint
 app.post('/login', (req, res) => {
-    req.session.user = {
-        'name': req.body.name,
-        'pass': req.body.pass
-    };
+	req.session.user = {
+		name: req.body.name,
+		password: req.body.pass
+	}
     res.redirect('/userhome');
 });
 
@@ -63,9 +60,12 @@ app.get('/logout', (req, res) => {
 // get userhome endpoint
 // Use 'auth' middleware when the page requires authentication
 app.get('/userhome', auth, (req, res) => {
-    let page = `
+	let page = `
+		<h2>
          Hi, ${req.session.user.name}!<br>
-         This is User's Home Page!
+		 This is the User Home Page!<br>
+		 <a href="/logout">Logout</a>
+		 <h2>
         `
     res.send(page);
 });
@@ -76,13 +76,16 @@ app.get('/content', auth, (req, res) => {
     let page = `
          Hi, ${req.session.user.name}!<br>
          You are now browsing this page because you've logged in.
-        `
+        `;
     res.send(page);
 });
 
 // authentication failed
 app.get('/error', (req, res) => {
-    res.send("login failed - unauthorized access");
+	res.status(401).send(`
+	<h2>Unauthorized access: Login failed or Access to protected page</h2>
+	<a href="/login">Go to Login page</a>
+	`);
 });
 
 const PORT = process.env.PORT || 3000;
